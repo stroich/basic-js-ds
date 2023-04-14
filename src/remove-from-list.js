64220ea9cb6d417aa2335bac6934fb3e8cf0723f
stroich <stroichmwork@gmail.com>
2023-04-14 20:22:39 +0400

    feat: add remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -8,7 +8,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * @param {List} l
  * @param {Number} k
- * @return {List}
+ * @return {*[]}
  *
  * @example
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
@@ -22,9 +22,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let list = new ListNode(0);
+  function createList(list,l,k){
+    if(l.value!==k){
+      if(l.next===null || (l.next.next==null && l.next.value===k)){
+        list.value = l.value;
+      }else{
+        let element =new ListNode(0);
+        list.value = l.value;
+        list.next = element;
+      }
+      if (l.next) {
+        createList(list.next, l.next, k);
+      }
+    }else {
+      if (l.next) {
+        createList(list, l.next, k);
+      }
+    }
+    return list;
+  }
+  let result = createList(list, l,k);
+  return result;
+
 }
 
 module.exports = {
